9a73fce997c065fecfaa49d73dea9b48af6ff1aa
Maxime <maximebatista18@gmail.com>
2021-10-10 15:56:12 +0200

    Stream: Fixed and optimised RemoteFileReader and RemoteFileWriter

--- Stream/src/main/scala/fr/overrride/scs/stream/RemoteFileReader.scala
+++ Stream/src/main/scala/fr/overrride/scs/stream/RemoteFileReader.scala
@@ -1,8 +1,8 @@
 package fr.overrride.scs.stream
 
 import fr.overrride.scs.common.fs.FileStoreItemInfo
-import fr.overrride.scs.common.packet.{EOFPacket, FileSegmentPacket, ObjectPacket}
 import fr.overrride.scs.common.packet.exception.{FileCollapseException, UnexpectedPacketException}
+import fr.overrride.scs.common.packet.{EOFPacket, FileSegmentPacket, ObjectPacket}
 
 import java.io.OutputStream
 import java.nio.file.attribute.FileTime
@@ -24,20 +24,22 @@ class RemoteFileReader(in: PacketInputStream) {
         val path  = info.relativePath
         while (true) {
             next match {
-                case EOFPacket                           => return total
-                case FileSegmentPacket(fn, len, segment) =>
+                case EOFPacket                           =>
+                    out.close()
+                    return total
+                case FileSegmentPacket(fn, segment) =>
                     if (fn != info.relativePath)
                         throw new FileCollapseException(s"Received foreign file fragment while downloading file $path. (received fragment from file $fn)")
-                    writeContent(out, segment, len)
+                    writeContent(out, segment)
                     total += segment.length
                     next = in.readPacket()
             }
         }
-        total
+        total //impossible
     }
 
-    protected def writeContent(out: OutputStream, segment: Array[Byte], len: Int): Unit = {
-        out.write(segment, 0, len)
+    protected def writeContent(out: OutputStream, segment: Array[Byte]): Unit = {
+        out.write(segment)
     }
 
     private def nextFileInfo(): FileStoreItemInfo = {
--- Stream/src/main/scala/fr/overrride/scs/stream/RemoteFileWriter.scala
+++ Stream/src/main/scala/fr/overrride/scs/stream/RemoteFileWriter.scala
@@ -2,33 +2,48 @@ package fr.overrride.scs.stream
 
 import fr.overrride.scs.common.fs.FileStoreItemInfo
 import fr.overrride.scs.common.packet.{EOFPacket, FileSegmentPacket, ObjectPacket}
+import fr.overrride.scs.stream.RemoteFileWriter.SegmentSize
 
 import java.io.InputStream
 import java.nio.file.{Files, Path}
 
 class RemoteFileWriter(out: PacketOutputStream) {
 
-    def writeFile(source: Path, cloudPath: String, segmentSize: Int): Unit = {
+    def writeFile(source: Path, cloudPath: String): Unit = {
         val info = writeFileInfo(source, cloudPath)
-        writeFileContent(source, info, segmentSize)
+        writeFileContent(source, info)
     }
 
-    private def writeFileContent(source: Path, info: FileStoreItemInfo, segmentSize: Int): Unit = {
-        val buff  = new Array[Byte](segmentSize)
-        val in    = Files.newInputStream(source)
-        var count = readContent(in, buff)
-        while (count != -1) {
-            out.writePacket(FileSegmentPacket(info.relativePath, count, buff))
-            count = readContent(in, buff)
+    private def writeFileContent(source: Path, info: FileStoreItemInfo): Unit = {
+        val in      = Files.newInputStream(source)
+        val size    = Math.min(in.available(), SegmentSize)
+        var content = readContent(in, size)
+        while (content.nonEmpty) {
+            out.writePacket(FileSegmentPacket(info.relativePath, content))
+            content = readContent(in, size)
         }
         out.writePacket(EOFPacket)
     }
 
-    protected def readContent(in: InputStream, buff: Array[Byte]): Int = in.read(buff)
+    protected def readContent(in: InputStream, len: Int): Array[Byte] = {
+        val buff  = new Array[Byte](len)
+        val count = in.read(buff)
+        if (count == -1)
+            return Array.empty
+        val result = new Array[Byte](count)
+        System.arraycopy(buff, 0, result, 0, count)
+        result
+    }
 
     private def writeFileInfo(file: Path, remotePath: String): FileStoreItemInfo = {
         val info = FileStoreItemInfo(remotePath, Files.isDirectory(file), Files.getLastModifiedTime(file).toMillis)
         out.writePacket(ObjectPacket(info))
         info
     }
+
+}
+
+object RemoteFileWriter {
+
+    private final val SegmentSize: Int = 150000
 }
